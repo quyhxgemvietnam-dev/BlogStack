12d86704614309065a56ce31ac47414fa96a475a
QuyHX <quyhx.gemvietnam@gmail.com>
2025-09-05 10:04:37 +0700

    Init Blog Stack

---
+++ src/main/java/com/example/BlogStack/BlogStackApplication.java
@@ -0,0 +1,13 @@
+package com.example.BlogStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BlogStack/BlogStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BlogStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
